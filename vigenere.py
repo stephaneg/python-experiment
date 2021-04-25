131998033aecb7f8f3092b7580e009bad9840c48
Stephane <stephstephg@gmail.com>
2021-04-25 15:28:16 +0200

    version simple qui fonctionne

--- vigenere.py
+++ vigenere.py
@@ -3,8 +3,10 @@ Exemple de codage / decodage avec la methode de Vigenere
 """
 
 
-alphabet = ["A", "B", "C", "D", "E", "F", "G", "H"]
-tableVigenere = []
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+table = []
+
+
 
 # decale une liste d'éléments d'alphabet d'un élément vers la droite
 def decale(alpha):
@@ -13,47 +15,82 @@ def decale(alpha):
     newAlpha =  alphaDroit + alphaGauche
     return newAlpha
 
-
+# renvoie l'indice d'une lettre dans l'alphabet
 def indexAlphabet(lettre):
     return alphabet.index(lettre)
 
+# renvoie la lettre située en ligne, colonne dans la table de Vigenere
+def findInTable(ligne, colonne):
+    return table[ligne][colonne]
 
 # genere la matrice de Vigenere à partir d'un alphabet
 def creeTableVigenere():
     currentAlpha = alphabet
     for i in range(len(alphabet)):
-        tableVigenere.append(currentAlpha)
+        table.append(currentAlpha)
         currentAlpha = decale(currentAlpha)
     print("table de Vigenere initialisee")    
-    return tableVigenere
 
 
+
+# code le texte selon la clé
 def code(texte, cle):
-    """
-    encode un texte avec la clé donnée
-    """
     longueur_cle = len(cle)
     lettre_cle = ""
     position_cle=0
+    code=""
 
     # on itere sur toutes les lettres du texte
     for lettre in texte:
-        print(lettre)
-
         # on determine la lettre de la clé
         lettre_cle = cle[position_cle]
-        print("lette de la cle : {}".format(lettre_cle))
 
         # on fait avancer la position de la clé
         position_cle = position_cle+1
         if position_cle>=longueur_cle :
             position_cle = 0
 
-        # on determine la ligne a utiliser dans la table de Vigenere
+        # on determine la ligne et la colonne a utiliser dans la table de Vigenere
         ligne = indexAlphabet(lettre_cle)
-        print(ligne)
+        colonne = indexAlphabet(lettre)
+        code = code + findInTable(ligne, colonne)
+
+    return code 
 
 
-# initialise la table de Vigenere
-creeTableVigenere()
+# decode le texte selon la clé
+def decode(texte, cle):
+    longueur_cle = len(cle)
+    lettre_cle = ""
+    position_cle = 0
+    decode = ""
 
+    # on itere sur toutes les lettres du texte
+    for lettre in texte:
+        # on determine la lettre de la clé
+        lettre_cle = cle[position_cle]
+
+        # on fait avancer la position de la clé
+        position_cle = position_cle+1
+        if position_cle >= longueur_cle:
+            position_cle = 0
+
+        # on determine la ligne et la colonne a utiliser dans la table de Vigenere
+        ligne = indexAlphabet(lettre_cle)
+        colonne = table[ligne].index(lettre)
+        decode = decode + alphabet[colonne]
+
+    return decode
+
+
+def main():
+    print("Code de Vigenere")
+    print("alphabet : {} de taille {}".format(alphabet, len(alphabet)))
+    # initialise la table de Vigenere
+    creeTableVigenere()
+
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
